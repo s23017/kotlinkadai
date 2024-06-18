08c6575e7389eae70b5d1dc2f28245631c12d8ba
s23017 <s23017@std.it-college.ac.jp>
2024-06-18 15:14:20 +0900

    firstcommit

---
+++ src/main/kotlin/jp/ac/it_college/std/s23017/message/board/MessageBoardApplication.kt
@@ -0,0 +1,11 @@
+package jp.ac.it_college.std.s23017.message.board
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MessageBoardApplication
+
+fun main(args: Array<String>) {
+    runApplication<MessageBoardApplication>(*args)
+}
---
+++ src/test/kotlin/jp/ac/it_college/std/s23017/message/board/MessageBoardApplicationTests.kt
@@ -0,0 +1,13 @@
+package jp.ac.it_college.std.s23017.message.board
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MessageBoardApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
