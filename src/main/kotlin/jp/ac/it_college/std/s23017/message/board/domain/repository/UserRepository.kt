5bd440658edc0649c4579c3487327f358ec766f1
s23017 <s23017@std.it-college.ac.jp>
2024-06-25 16:18:37 +0900

    UserRepository,UserRepositoryImpl作成

--- src/main/kotlin/jp/ac/it_college/std/s23017/message/board/domain/repository/UserRepository.kt
+++ src/main/kotlin/jp/ac/it_college/std/s23017/message/board/domain/repository/UserRepository.kt
@@ -2,10 +2,8 @@ package jp.ac.it_college.std.s23017.message.board.domain.repository
 
 import jp.ac.it_college.std.s23017.message.board.domain.model.User
 
-interface UserRepository{
-    fun findById(id: Long): User?
-    fun findByEmail(email: String): User?
+interface UserRepository {
+    fun find(email: String): User?
+    fun find(id: Long): User?
     fun save(user: User): User
-    fun update(user: User): User
-    fun delete(user: User)
 }
--- src/main/kotlin/jp/ac/it_college/std/s23017/message/board/infrastructure/database/repository/UserRepositoryImpl.kt
+++ src/main/kotlin/jp/ac/it_college/std/s23017/message/board/infrastructure/database/repository/UserRepositoryImpl.kt
@@ -5,50 +5,25 @@ import jp.ac.it_college.std.s23017.message.board.domain.repository.UserRepositor
 import jp.ac.it_college.std.s23017.message.board.infrastructure.database.dao.UserEntity
 import jp.ac.it_college.std.s23017.message.board.infrastructure.database.dao.UserTable
 import org.jetbrains.exposed.sql.transactions.transaction
+import org.springframework.stereotype.Repository
 
+@Repository
 class UserRepositoryImpl : UserRepository {
-
-    override fun findById(id: Long): User? {
-        return transaction {
-            val userEntity = UserEntity.findById(id)
-            userEntity?.toUser()
-        }
+    override fun find(email: String): User? = transaction {
+        UserEntity.find { UserTable.email eq email }.firstOrNull()?.toUser()
     }
 
-    override fun findByEmail(email: String): User? {
-        return transaction {
-            val userEntity = UserEntity.find { UserTable.email eq email }.firstOrNull()
-            userEntity?.toUser()
-        }
+    override fun find(id: Long): User? = transaction {
+        UserEntity.findById(id)?.toUser()
     }
 
-    override fun save(user: User): User {
-        return transaction {
-            val newUserEntity = UserEntity.new {
-                username = user.username
-                password = user.password
-                roleType = user.roleType
-            }
-            newUserEntity.toUser()
+    override fun save(user: User): User = transaction {
+        val userEntity = UserEntity.new(user.id) {
+            username = user.username
+            password = user.password
+            email = user.email
+            roleType = user.roleType
         }
-    }
-
-    override fun update(user: User): User {
-        return transaction {
-            val userEntity = UserEntity.findById(user.id) ?: throw IllegalArgumentException("User not found")
-            userEntity.apply {
-                username = user.username
-                password = user.password
-                roleType = user.roleType
-            }
-            userEntity.toUser()
-        }
-    }
-
-    override fun delete(user: User) {
-        transaction {
-            val userEntity = UserEntity.findById(user.id) ?: throw IllegalArgumentException("User not found")
-            userEntity.delete()
-        }
+        userEntity.toUser()
     }
 }
